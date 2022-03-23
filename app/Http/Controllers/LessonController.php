808c12b0d1ad0a975f694ada4c57f2b6bebc73cf
Debian <bitnami@ip-172-26-10-125.eu-central-1.compute.internal>
2022-03-23 16:17:49 +0000

    feat: add adding lesson in markdown format

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -4,13 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Lesson;
+use Parsedown;
 
 class LessonController extends Controller
 {
     public function addLesson(Request $req) {
         $lesson = new Lesson;
         $lesson->title = $req->input('lesson-title');
-        $lesson->content = $req->input('lesson-content');
+
+        if ($req->input('syntax-type') === 'markdown') {
+            $parsedown = new Parsedown();
+            $lesson->content = $parsedown->text($req->input('lesson-content'));
+        }
+        else {
+            $lesson->content = $req->input('lesson-content');
+        }
         
         $lesson->save();
   
--- resources/views/admin-dashboard.blade.php
+++ resources/views/admin-dashboard.blade.php
@@ -8,7 +8,6 @@
     <h3>Добавление урока</h3>
     <form action="{{ route('add-lesson-form') }}" method="post">
         @csrf
-
         <div class="form-group">
             <label for="lesson-title">Введите название урока</label>
             <input type="text" name="lesson-title" placeholder="Название урока" id="lesson-title" class="form-control">
@@ -16,7 +15,14 @@
         
         <div class="form-group">
             <label for="lesson-content">Введите содержание урока</label>
-            <textarea name="lesson-content" placeholder="Содержание урока" id="lesson-content" class="form-control"></textarea>
+            <textarea name="lesson-content" placeholder="Содержание урока" id="lesson-content" class="form-control" rows=20></textarea>
+        </div>
+
+        <div class="form-group">
+            <label for="syntax-type-html">html</label>
+            <input type="radio" name="syntax-type" id="syntax-type-html" value="html" checked>
+            <label for="syntax-type-markdown">Markdown</label>
+            <input type="radio" name="syntax-type" id="syntax-type-markdown" value="markdown">
         </div>
 
         <button type="submit" class="btn btn-success">Отправить</button>
